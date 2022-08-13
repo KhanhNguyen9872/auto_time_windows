eb9abc00b578f04bab284bc29a76b23cee744934
Nguyễn Văn Khánh <khanhvuinguyenvan800983609@gmail.com>
2022-08-13 10:32:50 +0700

    update v2
    add popup vcredist not installed
    add loop when curl not have internet
    fix error when Windows installed on other partition or disk

--- auto_time.bat
+++ auto_time.bat
@@ -11,36 +11,61 @@ net session >nul 2>&1
 if %errorLevel% == 0 (
     GOTO check_internet
 ) else (
+    echo Please run as administrator
+    pause
     exit /b
 )
 
 :check_internet
 ping google.com -n 1 -w 5000 >nul 2>&1
 if %errorLevel% == 0 (
-    GOTO HOME 
+    GOTO CHECK_CURL
 ) else (
     timeout 15 >nul 2>&1
     GOTO check_internet
 )
 
-:HOME
-.\KhanhNguyen9872\curl.exe -k -v --silent https://google.com 2> cache > NUL
-.\KhanhNguyen9872\grep.exe "< date:" "cache" | .\KhanhNguyen9872\sed.exe "s/< date: //" > cache_
-set /p datetime=<cache_
-echo %datetime% | .\KhanhNguyen9872\awk.exe "{print $5}" > cache
-set /p time=<cache
+:CHECK_CURL
+echo CreateObject("WScript.Shell").Popup "vcredist is not installed!", 99999, "Auto Time | KhanhNguyen9872" > khanh.vbs
+.\KhanhNguyen9872\curl.exe >cache 2>&1
+set /p curl=<cache
+if DEFINED curl (
+    GOTO GET_INFO
+) else (
+    WScript .\khanh.vbs
+    exit /b
+)
+
+:GET_INFO
+del /f .\khanh.vbs
 echo | date > cache
 .\KhanhNguyen9872\grep.exe -a -m1 "dd" "cache" | .\KhanhNguyen9872\awk.exe "{print $5}" | .\KhanhNguyen9872\sed.exe "s/(//g" | .\KhanhNguyen9872\sed.exe "s/)//g" | .\KhanhNguyen9872\sed.exe "s/dd/ /g" | .\KhanhNguyen9872\sed.exe "s/mm/ /g" | .\KhanhNguyen9872\sed.exe "s/yy/ /g" | .\KhanhNguyen9872\awk.exe "{print $1}" > cache_
 set /p khanh=<cache_
 .\KhanhNguyen9872\grep.exe -a -m1 "dd" "cache" | .\KhanhNguyen9872\awk.exe "{print $5}" | .\KhanhNguyen9872\sed.exe "s/(//g" | .\KhanhNguyen9872\sed.exe "s/)//g" | .\KhanhNguyen9872\sed.exe "s/%khanh%/ /g" > cache_
 set /p typedate=<cache_
-echo %datetime% | .\KhanhNguyen9872\awk.exe "{$1=$5=$6=Khanh; print $0}" | .\KhanhNguyen9872\sed.exe "s/Khanh//g" | .\KhanhNguyen9872\sed.exe "s/Jan/\/01\//g" | .\KhanhNguyen9872\sed.exe "s/Feb/\/02\//g" | .\KhanhNguyen9872\sed.exe "s/Mar/\/03\//g" | .\KhanhNguyen9872\sed.exe "s/Apr/\/04\//g" | .\KhanhNguyen9872\sed.exe "s/May/\/05\//g" | .\KhanhNguyen9872\sed.exe "s/Jun/\/06\//g" | .\KhanhNguyen9872\sed.exe "s/Jul/\/07\//g" | .\KhanhNguyen9872\sed.exe "s/Aug/\/08\//g" | .\KhanhNguyen9872\sed.exe "s/Sep/\/09\//g" | .\KhanhNguyen9872\sed.exe "s/Oct/\/10\//g" | .\KhanhNguyen9872\sed.exe "s/Nov/\/11\//g" | .\KhanhNguyen9872\sed.exe "s/Dec/\/12\//g" | .\KhanhNguyen9872\sed.exe "s/ //g" | .\KhanhNguyen9872\sed.exe "s/\// /g" > cache
-set /p date=<cache
 tzutil.exe /g > cache 2> NUL
 set /p current_utc=<cache
-GOTO READ_DATE
+GOTO CURL_TIME
 
-:READ_DATE
+:CURL_TIME
+.\KhanhNguyen9872\curl.exe -k -v --silent https://google.com 2> cache > NUL
+.\KhanhNguyen9872\grep.exe "< date:" "cache" | .\KhanhNguyen9872\sed.exe "s/< date: //" > cache_
+set /p datetime=<cache_
+if DEFINED datetime (
+    GOTO READ_DATETIME
+) else (
+    timeout 15 >nul 2>&1
+    GOTO CURL_TIME
+)
+
+:READ_DATETIME
+echo %datetime% | .\KhanhNguyen9872\awk.exe "{print $5}" > cache
+set /p time=<cache
+echo %datetime% | .\KhanhNguyen9872\awk.exe "{$1=$5=$6=Khanh; print $0}" | .\KhanhNguyen9872\sed.exe "s/Khanh//g" | .\KhanhNguyen9872\sed.exe "s/Jan/\/01\//g" | .\KhanhNguyen9872\sed.exe "s/Feb/\/02\//g" | .\KhanhNguyen9872\sed.exe "s/Mar/\/03\//g" | .\KhanhNguyen9872\sed.exe "s/Apr/\/04\//g" | .\KhanhNguyen9872\sed.exe "s/May/\/05\//g" | .\KhanhNguyen9872\sed.exe "s/Jun/\/06\//g" | .\KhanhNguyen9872\sed.exe "s/Jul/\/07\//g" | .\KhanhNguyen9872\sed.exe "s/Aug/\/08\//g" | .\KhanhNguyen9872\sed.exe "s/Sep/\/09\//g" | .\KhanhNguyen9872\sed.exe "s/Oct/\/10\//g" | .\KhanhNguyen9872\sed.exe "s/Nov/\/11\//g" | .\KhanhNguyen9872\sed.exe "s/Dec/\/12\//g" | .\KhanhNguyen9872\sed.exe "s/ //g" | .\KhanhNguyen9872\sed.exe "s/\// /g" > cache
+set /p date=<cache
+GOTO READ_TYPE_DATE
+
+:READ_TYPE_DATE
 echo %typedate% | .\KhanhNguyen9872\awk.exe "{print $%count%}" > cache_
 set /p temp=<cache_
 if %temp%==dd (set a=1)
@@ -50,7 +75,7 @@ echo %date% | .\KhanhNguyen9872\awk.exe "{print $%a%}" > cache_
 set /p temp=<cache_
 set final_date=%final_date% %temp%
 set /a "count=%count%+1"
-if %count%==4 (GOTO FINISH) else (GOTO READ_DATE)
+if %count%==4 (GOTO FINISH) else (GOTO READ_TYPE_DATE)
 
 :FINISH
 echo %final_date% | .\KhanhNguyen9872\sed.exe "s/ /%khanh%/g" | .\KhanhNguyen9872\sed.exe "s/^.//" | .\KhanhNguyen9872\sed.exe "s/.$//" > cache
--- install.bat
+++ install.bat
@@ -10,7 +10,10 @@ GOTO check_windows
 ver | .\KhanhNguyen9872\awk.exe "{print $4}" | .\KhanhNguyen9872\sed.exe "s/\./ /g" | .\KhanhNguyen9872\awk.exe "{print $1,$2}" | .\KhanhNguyen9872\sed.exe "s/ /./g" | .\KhanhNguyen9872\sed.exe "1d" > cache
 set /p version=<cache
 del /f cache >nul 2>&1
-if "%version%" == "10.0" (echo Windows 10) else (if "%version%" == "6.3" (echo Windows 8.1) else (if "%version%" == "6.2" (echo Windows 8) else (if "%version%" == "6.2" (echo Windows 8) else (if "%version%" == "6.1" (echo Windows 7) else (exit /b)))))
+if "%version%" == "10.0" (echo Windows 10) else (if "%version%" == "6.3" (echo Windows 8.1) else (if "%version%" == "6.2" (echo Windows 8) else (if "%version%" == "6.2" (echo Windows 8) else (if "%version%" == "6.1" (echo Windows 7) else (
+		echo Unsupported: Windows %version%
+		pause
+		exit /b)))))
 GOTO check_Permissions
 
 :check_Permissions
@@ -18,18 +21,19 @@ net session >nul 2>&1
 if %errorLevel% == 0 (
     GOTO INSTALL
 ) else (
+	echo Please run as administrator
     exit /b
 )
 
 :INSTALL
-set PATH_INSTALL=C:\Windows\AutoTime
+set PATH_INSTALL=%windir%\AutoTime
 rmdir /q /s %PATH_INSTALL% >nul 2>&1
 if not exist "%PATH_INSTALL%" (
 	mkdir %PATH_INSTALL% >nul 2>&1
 	mkdir %PATH_INSTALL%\KhanhNguyen9872 >nul 2>&1
 )
-move /Y ".\KhanhNguyen9872\*.*" "%PATH_INSTALL%\KhanhNguyen9872" >nul 2>&1
-move /Y ".\auto_time.exe" "%PATH_INSTALL%" >nul 2>&1
+copy /Y ".\KhanhNguyen9872\*.*" "%PATH_INSTALL%\KhanhNguyen9872" >nul 2>&1
+copy /Y ".\auto_time.exe" "%PATH_INSTALL%" >nul 2>&1
 echo Set WshShell = CreateObject("WScript.Shell" ) > "%ALLUSERSPROFILE%\Microsoft\Windows\Start Menu\Programs\StartUp\auto_time.vbs"
 echo WshShell.Run """%PATH_INSTALL%\auto_time.exe""", 0 >> "%ALLUSERSPROFILE%\Microsoft\Windows\Start Menu\Programs\StartUp\auto_time.vbs"
 echo Set WshShell = Nothing >> "%ALLUSERSPROFILE%\Microsoft\Windows\Start Menu\Programs\StartUp\auto_time.vbs"
@@ -49,5 +53,6 @@ exit /b
 :RUN_SERVICE
 echo.
 echo Please wait...
+echo.
 %PATH_INSTALL%\auto_time.exe
 exit /b
